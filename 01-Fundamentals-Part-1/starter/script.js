ede0f39781bcee85170315da83fbd2943f8e9001
shanghlxsg <shanghl@outlook.sg>
2022-08-15 17:27:56 +0800

    daily updates

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,51 @@
+/*
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+//if (js === "amazing") alert("JavaScript is FUN!");
 
-40+8+23-10;
+//40+8+23-10;
 console.log(40+8+23-10);
+
+let firstName = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Engineer";
+let myCurrentJob = "Coder";
+//compared to
+let job1 = "Engineer";
+let job2 = "Programmer";
+
+console.log(myCurrentJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1996.;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 21;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
